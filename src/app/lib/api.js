2533fbbeee94e3481f4c84c142daf69d4924ba95
mohamed sokkar <mohamed.h.sokkar@gmail.com>
2026-04-01 23:50:46 +0200

    a commit

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3007/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL //|| "http://localhost:3007/api";
 class ApiError extends Error {
   constructor(message, status) {
     super(message);
